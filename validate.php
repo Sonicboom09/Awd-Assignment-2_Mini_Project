ea232451f31983642fc513aa47768ac78ca94065
Sonicboom09 <72070683+Sonicboom09@users.noreply.github.com>
2022-04-19 13:42:07 +0530

    Update validate.php

--- validate.php
+++ validate.php
@@ -1,36 +1,60 @@
 <?php
+
+$user = 'root';
+$password = ''; 
   
-include_once('connection.php');
-   
+$database = 'loginpage'; 
+  
+$servername='localhost';
+$mysqli = new mysqli($servername, $user, 
+                $password, $database);
+  
+// Checking for connections
+if ($mysqli->connect_error) {
+    die('Connect Error (' . 
+    $mysqli->connect_errno . ') '. 
+    $mysqli->connect_error);
+}
+
+
+
 function test_input($data) {
-      
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+	
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
 }
-   
+
 if ($_SERVER["REQUEST_METHOD"]== "POST") {
-      
-    $adminname = test_input($_POST["adminname"]);
-    $password = test_input($_POST["password"]);
-    $stmt = $conn->prepare("SELECT * FROM adminlogin");
-    $stmt->execute();
-    $users = $stmt->fetchAll();
-      
-    foreach($users as $user) {
-          
-        if(($user['adminname'] == $adminname) && 
-            ($user['password'] == $password)) {
+	
+	$adminname = test_input($_POST["adminname"]);
+	$password = test_input($_POST["password"]);
+	$sql="SELECT * FROM adminlogin";
+    $result = $mysqli->query($sql);
+    $mysqli->close();
+	$i=0;
+	
+	while($row=$result->fetch_assoc())
+        {
+     $userid = $row['adminname'];
+     $pwd = $row['password'];
+	 if(($userid == $adminname) && 
+            ($pwd == $password)) {
                 header("Location: feedback.php");
+				$i=$i+1;
         }
-        else {
-            echo "<script language='javascript'>";
-            echo "alert('WRONG INFORMATION')";
-            echo "</script>";
-            die();
-        }
-    }
+	}
+		
+	if($i==0) {
+			echo "<script language='javascript'>";
+			echo "alert('WRONG INFORMATION')";
+			echo "</script>";
+			echo'<div>
+              <div style="text-align: center; font-size:200%;"><a href="http://localhost/htdocs/login.php" id="lnk100" title="form to email">Try Again</a></div>
+            </div>';
+		}
+	
 }
-  
+
 ?>
